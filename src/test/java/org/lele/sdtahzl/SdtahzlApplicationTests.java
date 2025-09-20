a4306708b8621b4f594c23983302d37422a81496
hanzongle <sdtahzl@163.com>
2025-09-20 13:04:29 +0800

    init project

---
+++ src/test/java/org/lele/sdtahzl/SdtahzlApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lele.sdtahzl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdtahzlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
